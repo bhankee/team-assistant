176f36326cd7c859a712cb75ee247387c32c9226
Brad Hankee <brad.hankee@gmail.com>
2018-09-27 16:34:48 -0400

    Set up Google OAuth and Mongoose.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.MongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('HI Team!');
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
